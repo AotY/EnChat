945a4be87692e2363eeb178de7bd751f64091841
AotY <694731929@qq.com>
2018-09-07 14:26:54 +0800

    pre.s2s

--- Classification/pre.s2s.sh
+++ Classification/pre.s2s.sh
@@ -2,10 +2,10 @@
 python tool/preprocess.py \
     -train_corpus_path /home/Research/EnChat/askqa/train.post.txt /home/Research/EnChat/askqa/train.response.txt /home/Research/EnChat/askqa/train.label.txt \
     -valid_corpus_path /home/Research/EnChat/askqa/valid.post.txt /home/Research/EnChat/askqa/valid.response.txt /home/Research/EnChat/askqa/valid.label.txt \
-    -valid_corpus_path /home/Research/EnChat/askqa/test.post.txt /home/Research/EnChat/askqa/test.response.txt //home/Research/EnChat/askqa/test.label.txt \
+    -valid_corpus_path /home/Research/EnChat/askqa/test.post.txt /home/Research/EnChat/askqa/test.response.txt /home/Research/EnChat/askqa/test.label.txt \
     -save_data data/reddit/askqa \
     -max_shard_size 153600000 \
-    -vocab_path data/reddit/vocab.pt \
+    -vocab_path data/reddit/askqa/vocab.pt \
     -seq_length 80 \
     -vocab_size 100000 \
     -with_label \
