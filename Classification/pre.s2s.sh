6f20c8e766e8dd75a5c10b1c2231358daabd5389
AotY <694731929@qq.com>
2018-09-07 11:44:36 +0800

    modify pre sh

--- Classification/pre.s2s.sh
+++ Classification/pre.s2s.sh
@@ -1,2 +1,19 @@
-#python preprocess.py -train_corpus_path /home/xuzhen/Project/dataset/opensub/open_train_post.txt /home/xuzhen/Project/dataset/opensub/open_train_response.txt -valid_corpus_path /home/xuzhen/Project/dataset/opensub/open_valid_post.txt /home/xuzhen/Project/dataset/opensub/open_valid_response.txt -save_data data/opensub/opensub -max_shard_size 15360000 -vocab_path data/opensub/vocab.pt -seq_length 15 -vocab_size 50000 -pre_word_vecs_path /home/xuzhen/Project/dataset/w2v/glove_twitter/glove.twitter.27B.100d.txt 
-python preprocess.py -train_corpus_path /home/xuzhen/Project/dataset/douban/gan/train_post.txt /home/xuzhen/Project/dataset/douban/gan/train_response.txt -valid_corpus_path /home/xuzhen/Project/dataset/douban/gan/valid_post.txt /home/xuzhen/Project/dataset/douban/gan/valid_response.txt -save_data data/douban/douban -max_shard_size 15360000 -vocab_path data/douban/vocab.pt -seq_length 15 -vocab_size 50000 -pre_word_vecs_path /home/xuzhen/Project/dataset/douban/w2v/chat.w2v.35w.100d.clean.txt 
+#!/usr/bin/env bash
+python preprocess.py \
+    -train_corpus_path /home/Research/EnChat/askqa/train.post.txt /home/Research/EnChat/askqa/train.response.txt /home/Research/EnChat/askqa/train.label.txt \
+    -valid_corpus_path /home/Research/EnChat/askqa/valid.post.txt /home/Research/EnChat/askqa/valid.response.txt /home/Research/EnChat/askqa/valid.label.txt \
+    -valid_corpus_path /home/Research/EnChat/askqa/test.post.txt /home/Research/EnChat/askqa/test.response.txt //home/Research/EnChat/askqa/test.label.txt \
+    -save_data data/reddit \
+    -max_shard_size 153600000 \
+    -vocab_path data/reddit/vocab.pt \
+    -seq_length 80 \
+    -vocab_size 100000 \
+    -with_label \
+    -pre_word_vecs_path /home/taoqing/Research/data/GoogleNews-vectors-negative300.bin \
+    -pre_word_vecs_type word2vec \
+    -pre_word_vecs_dim 300 \
+    -pre_word_vecs_file_type binary \
+
+
+/
+
